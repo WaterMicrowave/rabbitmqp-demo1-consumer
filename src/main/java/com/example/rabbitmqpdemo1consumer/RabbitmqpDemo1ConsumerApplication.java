97e589b04db19d7ed5296b89bfd718ca15021344
lixiangping <admin>
2022-01-09 13:55:12 +0800

    Initial commit

---
+++ src/main/java/com/example/rabbitmqpdemo1consumer/RabbitmqpDemo1ConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqpdemo1consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqpDemo1ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqpDemo1ConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rabbitmqpdemo1consumer/RabbitmqpDemo1ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqpdemo1consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqpDemo1ConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
